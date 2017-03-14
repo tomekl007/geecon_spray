f5e75b7d32966fbd71d942de6df2e30284d9a5c7
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-03-14 15:06:23 +0100

    more descriptive comment

--- src/main/scala/com/example/actors/ReportGeneratorActor.scala
+++ src/main/scala/com/example/actors/ReportGeneratorActor.scala
@@ -3,10 +3,14 @@ package com.example.actors
 import akka.actor.Actor
 import com.example.actors.messages.TriggerGenerating
 import spray.caching.Cache
+import spray.http.DateTime
 
 class ReportGeneratorActor(cache: Cache[String]) extends Actor {
   implicit val ec = context.dispatcher
+
   override def receive: Receive = {
-    case TriggerGenerating(accountId, reportType) => cache(accountId + reportType) {"reportContent" }
+    case TriggerGenerating(accountId, reportType) => cache(accountId + reportType) {
+      s"reportContent that was generated ${DateTime.now}"
+    }
   }
 }
